a29b01a7292229b97ed1b7aec9efb41b80527bd1
gonghenghai <1014921649@qq.com>
2020-11-19 17:50:53 +0800

    test2

---
+++ Attendance/Program.cs
@@ -0,0 +1,20 @@
+﻿using Attendance.Controller.Analysis;
+using Attendance.Controller.ExcelToSQL;
+
+namespace Attendance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //testclass method = new testclass();
+            //method.test();
+            excelToSQLMain a = new excelToSQLMain();
+            a.ImportAttendanceInfoToMySQL("D:/", "test.xlsx");
+            a.ImportHolidayChangesToMySQL("D:/", "holiday.xlsx");
+            a.ImportSkipEmployeeToMySQL("D:/", "skip.xlsx");
+            //analysisMain b = new analysisMain();
+            //b.AnalysisResultsToMySQL();
+        }
+    }
+}
