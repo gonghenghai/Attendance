4f5af75c9624cdfb03b9ca1be4ac10085f89e407
gonghenghai <1014921649@qq.com>
2020-11-20 18:10:49 +0800

    测试。。。

--- Attendance/Program.cs
+++ Attendance/Program.cs
@@ -10,9 +10,9 @@ namespace Attendance
             //testclass method = new testclass();
             //method.test();
             excelToSQLMain a = new excelToSQLMain();
-            a.ImportAttendanceInfoToMySQL("D:", "test.xlsx");
-            a.ImportHolidayChangesToMySQL("D:", "holiday.xlsx");
-            a.ImportSkipEmployeeToMySQL("D:", "skip.xlsx");
+            a.ImportAttendanceInfoToMySQL("D:", "files/test.xlsx");
+            a.ImportHolidayChangesToMySQL("D:", "files/holiday.xlsx");
+            a.ImportSkipEmployeeToMySQL("D:", "files/skip.xlsx");
             //analysisMain b = new analysisMain();
             //b.AnalysisResultsToMySQL();
         }
